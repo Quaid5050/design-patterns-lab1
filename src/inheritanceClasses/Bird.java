ce80be59b8101b32115df1d6adac95e7b14cd33e
Quaid Ahmed <inoxent5050@gmail.com>
2023-09-07 10:41:38 +0500

    Implement Static polymorphism

--- src/inheritanceClasses/Bird.java
+++ src/inheritanceClasses/Bird.java
@@ -22,4 +22,14 @@ public class Bird extends Animal {
         System.out.println("Bird is flying down.");
     }
 
+    public void  fly()  {
+        System.out.println("The bird is flying.");
+    }
+    public  void  fly(int height)  {
+        System.out.println("The bird is flying "  + height +  " feet high.");
+    }
+    public  void  fly(String name,  int height)  {
+        System.out.println("The "  + name +  " is flying "  + height +  " feet high.");
+    }
+
 }
